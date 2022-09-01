242e06b0bd2fbb4fac980b5a78e8504ced8b1302
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-01 15:06:27 -0300

    connecting mongodb to node

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-
 import express from 'express';
+import cors from 'cors'
+import { MongoClient } from 'mongodb';
 
-const server = express()
+const app = express()
+const mongoClient = new MongoClient('mongodb://localhost:27017')
+let db
+app.use(express.json())
+app.use(cors())
 
-server.listen(5000, () => console.log('Listening on port 5000'))
+mongoClient.connect().then(() =>{
+    db = mongoClient.db('bate-papo')
+})
+
+app.listen(5000, () => console.log('Listening on port 5000'))
