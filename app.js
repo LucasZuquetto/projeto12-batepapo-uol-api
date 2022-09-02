c4db2e185c16a857df71c1df0203f2e9a41abe71
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-09-02 16:20:22 -0300

    fix: db connection

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv'
 import dayjs from 'dayjs';
 
+//post /status
+//remoção automatica de usuarios
+//validação com mongo e com joy
+//bonus
+//prettier
+//refatorar com async await
+//o que é new do javascript 
+
 dotenv.config()
 const app = express()
 const mongoClient = new MongoClient(process.env.MONGO_URI)
-let dbParticipants
-let dbMessages
+let db
 app.use(express.json())
 app.use(cors())
 
 mongoClient.connect().then(() =>{
-    dbParticipants = mongoClient.db('UolParticipants')
-})
-mongoClient.connect().then(() =>{
-    dbMessages = mongoClient.db('UolMessages')
+    db = mongoClient.db('Bate-Papo-Uol')
 })
 
 app.post('/participants', (req,res) =>{
     const { name } = req.body
-    dbParticipants.collection('UolParticipants').insertOne({
+    db.collection('Uol-Participants').insertOne({
         name:name,
         lastStatus: Date.now()
     })
@@ -29,7 +33,7 @@ app.post('/participants', (req,res) =>{
 })
 
 app.get('/participants', (req,res) => {
-    dbParticipants.collection('UolParticipants').find().toArray().then((users)=>res.send(users))
+    db.collection('Uol-Participants').find().toArray().then((users)=>res.send(users))
 })
 
 app.post('/messages', (req,res) => {
@@ -39,7 +43,7 @@ app.post('/messages', (req,res) => {
         ...req.body,
         from:user
     }
-    dbMessages.collection('UolMessages').insertOne({
+    db.collection('Uol-Messages').insertOne({
         ...messageObject,
         time:time
     })
@@ -50,9 +54,9 @@ app.get('/messages', (req,res) => {
     const limit = parseInt(req.query.limit)
     const user = req.headers.user
     if(!limit){
-        dbMessages.collection('UolMessages').find({$or:[{from:user},{to:user},{to:'Todos'}]}).toArray().then((messages) => res.send(messages))
+        db.collection('Uol-Messages').find({$or:[{from:user},{to:user},{to:'Todos'}]}).toArray().then((messages) => res.send(messages))
     }else{
-        dbMessages.collection('UolMessages').find({$or:[{from:user},{to:user},{to:'Todos'}]}).toArray().then((messages) => res.send(messages.slice(-limit)))
+        db.collection('Uol-Messages').find({$or:[{from:user},{to:user},{to:'Todos'}]}).toArray().then((messages) => res.send(messages.slice(-limit)))
     }
 })
 
